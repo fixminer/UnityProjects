af56239e4d83da96eab3de3f64ef2d24a4d22101
fixminer <118664331+fixminer@users.noreply.github.com>
2022-11-27 16:29:45 +0400

    Add files via upload

---
+++ NPC.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+	int health = 100;
+	int damage = 10;
+	float speed = 0.5f;
+	private GameObject target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
